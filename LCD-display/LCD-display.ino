e3ef4a8b09ba62d0fa4ca951304d52e8a39e9aa9
nigel-jones-uni <161096477+NigelJonesBakedGoods@users.noreply.github.com>
2025-02-18 01:00:54 +1300

    Not Tested yet. Temp sens pin needs work
    i actually started it

--- LCD-display/LCD-display.ino
+++ LCD-display/LCD-display.ino
@@ -1,9 +1,35 @@
-void setup() {
-  // put your setup code here, to run once:
+/*
+  The Fish tank parameter tracker is a program designed for the Arduino mega 2560 to track different variable in my fish tank.
+*/
+#include <LiquidCrystal.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
+// Setup for Temp sensor
+#define ONE_WIRE_BUS 22
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+
+// Setup for LCD display
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+
+void setup() {
+  sensors.begin();
+  Serial.begin(9600);
+  lcd.begin(16, 2);
+  lcd.print("Hello, World!");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
 
 }
+
+//prints temp in C
+void printTemp() {
+  lcd.setCursor(0, 1);
+  lcd.print(sensors.getTempCByIndex(0)+ ((char)176) + "C");
+}
+
