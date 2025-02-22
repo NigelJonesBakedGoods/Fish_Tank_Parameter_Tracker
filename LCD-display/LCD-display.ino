318abb040429d6032b4cc7932226e35fc1cf9a28
NigelonesBakedGoods <161096477+NigelJonesBakedGoods@users.noreply.github.com>
2025-02-23 00:32:10 +1300

    New time cycle method

--- LCD-display/LCD-display.ino
+++ LCD-display/LCD-display.ino
@@ -14,8 +14,20 @@ DallasTemperature sensors(&oneWire);
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
+const int cycleButton = 24;
+const int resetButton = 25;
+int menuCycle;
+
+//reset times in day, week order
+const int time24H = 86400000;
+const int time168H = 604800000;
+int foodTime = time24H;
+int waterChangeTime = time168H;
+
 
 void setup() {
+  pinMode(cycleButton, INPUT_PULLUP);
+  pinMode(resetButton,  INPUT_PULLUP);
   sensors.begin();
   Serial.begin(9600);
   lcd.begin(16, 2);
@@ -23,13 +35,44 @@ void setup() {
 }
 
 void loop() {
-  
+  if (digitalRead(resetButton) == HIGH){
+    menuCycle = (menuCycle + 1) % 5;
+  }
+  foodTime = time24H;
+  if (waterChangeTime == 0){
+    waterChangeTime = time168H;
+  }
+  foodTime -= 10;
+  waterChangeTime -= (waterChangeTime - 10);
+  changeFoodTime(foodTime);
+  changeWaterChangeTime(waterChangeTime);
 
 }
 
+void changeFoodTime(int(time)){
+
+}
+
+void displayFoodTime(int(time)){
+
+}
+
+void changeWaterChangeTime(int(time)){
+
+}
+
+void displayWaterChangeTime(int(time)){
+
+}
+
+
+
+/*
 //prints temp in C
 void printTemp() {
   lcd.setCursor(0, 1);
   lcd.print(sensors.getTempCByIndex(0)+ ((char)176) + "C");
-}
 
+
+}
+*/
